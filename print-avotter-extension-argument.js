5370780f0c7b0bdcbee69ea5044a887b64b50b3a
avotoko <53560467+avotoko@users.noreply.github.com>
2019-09-13 10:35:16 +0900

    あぼったー拡張引数表示

--- print-avotter-extension-argument.js
+++ print-avotter-extension-argument.js
@@ -1,26 +1,46 @@
 /*
+ * あぼったー拡張引数表示 v.0.1.1
  * あぼったー拡張の hideThisTweet に渡される引数 data の内容を表示します
  * 開発（デベロッパー）ツールのコンソールにコピペして実行してみてください
  * この拡張を無効化するには avotter.addAddon({name: "あぼったー拡張引数表示"}) を実行します
 */
 
 (function(){
+	function log(s, css)
+	{
+		console.log.apply(console, arguments);
+	}
+
 	function quote(s)
 	{
-		return typeof s === "string" ? ('"' + s.replace(/\\/g,"\\\\").replace(/\n/g,"\\n").replace(/"/g,"\\\"") + '"') : s.toString();
+		return '"' + s.replace(/\\/g,"\\\\").replace(/\n/g,"\\n").replace(/"/g,"\\\"") + '"';
 	}
 	
-	function hideThisTweet(data, state)
+	function printObject(obj, depth)
 	{
-		console.log("{");
-		Object.keys(data).forEach(k=>{
-			console.log("    " + k + ": " + quote(data[k]));
+		if (depth == null)
+			depth = 0;
+		depth === 0 && log("{");
+		function indent(){return "    ".repeat(depth+1)}
+		Object.keys(obj).forEach(k=>{
+			function pre(){return indent() + k + ": "}
+			let v = obj[k];
+			if (v === null)
+				log(pre() + "null");
+			else if (typeof v === "string")
+				log(pre() + quote(v));
+			else if (typeof v === "object" && v.constructor.name === "Object")
+				log(pre() + "{"), printObject(v, depth+1), log(indent() + "}");
+			else
+				log(pre() + v.toString());
 		});
-		console.log("}");
+		depth === 0 && log("}");
 	}
-	
+
 	avotter.addAddon({
 		name: "あぼったー拡張引数表示",
-		hideThisTweet: hideThisTweet
+		hideThisTweet: function (data, state){
+			printObject(data);
+		}
 	});
 })();
